b3ae39fb74cd06ed02904558e753ef6eed22b2c0
khhan <khhan@umich.edu>
2024-10-20 14:37:08 -0400

    Create function that cleans lines of comments and docstrings with regex

---
+++ src/model/preprocess.py
@@ -0,0 +1,9 @@
+import regex as re
+from datasets import load_dataset, Dataset, IterableDataset
+
+        
+# Clean inline comments and block comments
+def clean_comments(code: str) -> str:
+    no_comment = re.sub(r'#[^\n]*', '', code)                   # remove comments '# comment'
+    no_docstring = re.sub(r'\n\s*"""[^(""")]+"""', no_comment)  # remove docstrings '"""docstring"""'
+    return no_docstring
