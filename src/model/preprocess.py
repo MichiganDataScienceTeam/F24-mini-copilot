a59994fc2eea4f4856d494b07029f48995632f8d
maxim12313 <maximkim16@gmail.com>
2024-11-01 02:08:34 -0400

    improved regex cleaning, but approach is sketchy. Using intermediate regex flag value.

--- src/model/preprocess.py
+++ src/model/preprocess.py
@@ -1,17 +1,36 @@
+from collections.abc import MutableSet
 import regex as re
 from datasets import load_dataset, Dataset
 
 
+# Im not sure this was the best approach for doc comments
 # Clean inline comments and block comments
 def clean_comments(code: str) -> str:
-    no_comment = re.sub(r"#[^\n]*", "", code)  # remove comments '# comment'
+    # remove comments with #
+    no_comment = re.sub(r"#[^\n]*", "", code)
 
-    # remove doc strings (limited to be following def or class)
-    # https://stackoverflow.com/questions/1687620/regex-match-everything-but-a-specific-pattern
-    no_docstring = re.sub(
-        r'(class|def)(.+)\s+"""((?:(?!""")[\s\S])*)"""', "", no_comment
+    string_pre = r"(\S+\s*=\s*)"
+    between = r'(""")((?:(?!""")[\s\S])*)(""")'
+
+    # this should not hit
+    holder = "!|<1multiline1>|!"
+
+    # initially save mutliline strings with flag value
+    save_strings = re.sub(
+        string_pre + between,
+        rf"\1{holder}\3{holder}",
+        no_comment,
     )
-    return re.sub(r"\s*\n", "\n", no_docstring)  # remove trailing whitespace
+
+    # removing docs without caring about mutliline
+    remove_docs = re.sub(between, "", save_strings)
+
+    # fix flags back to original
+    fix_strings = re.sub(holder, '"""', remove_docs)
+
+    # get rid of trailing
+    no_trailing = re.sub(r"\s*\n", "\n", fix_strings)
+    return no_trailing
 
 
 def include(content: str) -> bool:
@@ -26,7 +45,7 @@ def include(content: str) -> bool:
     ]
     for library in libraries:
         a = "import " + library
-        b = "from " + library
+        b = "from " + library + " import"
         if a in content or b in content:
             return True
     return False
@@ -39,12 +58,13 @@ def get_data():
         trust_remote_code=True,
         split="train",
     )
-    ds = ds.map(lambda x: {"content": clean_comments(x["content"])})
+
     ds = ds.filter(lambda x: include(x["content"]))
+    ds = ds.map(lambda x: {"content": clean_comments(x["content"])})
     return ds
 
 
-if __name__ == "__main__":
+def preview():
     ds = get_data()
     i = 0
     for x in ds:
@@ -52,3 +72,7 @@ if __name__ == "__main__":
         i += 1
         if i == 100:
             break
+
+
+if __name__ == "__main__":
+    pass
