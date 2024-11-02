ab93b688467b83aaeffbb7c9bef5deab1ae484db
Jeffrey Lu <jeffrey.p.lu@gmail.com>
2024-11-01 21:35:44 -0400

    Chunking and ChunkedDataset

--- src/model/chunk.py
+++ src/model/chunk.py
@@ -8,29 +8,47 @@ def chunk(inp: str,
           tokenizer: AutoTokenizer, 
           chunk_size: int = 256, 
           overlapping_len: int = 3,
-          num_chunks: int = 128) -> list:
-    tokenized_txt = tokenizer(inp, return_tensors="pt")["input_ids"]
-    token_len = tokenized_txt.size()[1]
+          max_chunks: int = 128) -> torch.Tensor:
     
-    if token_len <= chunk_size:
-        return [tokenized_txt]
-    else:
-        chunks = []
-        chunks.append(tokenized_txt.view(-1)[:chunk_size])
-        for i in range(chunk_size, token_len, chunk_size):
-            if len(chunks) == num_chunks:
-                # only return num_chunks chunks so that it is not stuck on the same sample for too long
-                break
-            if i + chunk_size > token_len:
-                new_chunk = tokenized_txt.view(-1)[i-overlapping_len:]
-                new_chunk_len = new_chunk.size()[0]
-                new_chunk = new_chunk.view(1, new_chunk_len)
-                pad_len = chunk_size - new_chunk_len
-                padded_chunk = torch.cat((new_chunk, torch.full((1, pad_len), tokenizer.eos_token_id)), dim=1)
-                padded_chunk = padded_chunk.view(-1)
-                chunks.append(padded_chunk)
-                continue
-            else:
-                new_chunk = tokenized_txt.view(-1)[i-overlapping_len:i+chunk_size-overlapping_len]
-                chunks.append(new_chunk)
-        return chunks
+    # Tokenize entire sample
+    tokenized_txt = tokenizer(inp,
+                              return_tensors="pt"
+                             )["input_ids"].view(-1)
+    token_len = len(tokenized_txt)
+    
+    # Add chunks
+    chunks = []
+    last_padding_size = 0
+
+    for i in range(0, token_len-overlapping_len, chunk_size-overlapping_len):
+        # Exit if max_chunks is met
+        if len(chunks) >= max_chunks:
+            break
+
+        # Create (potentially too short) new chunk
+        new_chunk = tokenized_txt[i:i+chunk_size]
+        
+        # Generate (potentially empty) padding
+        padding = torch.full(
+            size=(chunk_size - len(new_chunk), ),
+            fill_value=tokenizer.pad_token_id if tokenizer.pad_token_id else tokenizer.eos_token_id
+        )
+        last_padding_size = max(0, chunk_size - len(new_chunk))
+
+        # Pad
+        new_chunk = torch.cat((new_chunk, padding))
+        
+        # Add new correctly-sized chunk
+        chunks.append(new_chunk)
+    
+    # Compile results
+    input_ids = torch.stack(chunks)
+    attention_mask = torch.ones_like(input_ids)
+
+    if last_padding_size >= 2:
+        attention_mask[-1, -last_padding_size:] = 0
+
+    return {
+        "input_ids": input_ids,
+        "attention_mask": attention_mask
+    }
--- src/model/dataset.py
+++ src/model/dataset.py
@@ -1,8 +1,11 @@
 from datasets import load_dataset
-from torch.utils.data import DataLoader, IterableDataset
+from torch.utils.data import IterableDataset
+from transformers import AutoTokenizer
 from typing import Iterator
 
 from preprocess import clean_comments, include, keep_only_content
+
+from chunk import chunk
 
 class CleanDataset(IterableDataset):
     TRAIN_SPLIT_NAME = "codeparrot/codeparrot-clean-train"
@@ -18,7 +21,7 @@ class CleanDataset(IterableDataset):
         ds = load_dataset(SPLIT_NAME,
                           streaming=True,
                           split="train")    # Invariant for BOTH train and val sets
-    
+
         # Preprocessing
         ds = ds.filter(lambda x: x["path"].endswith(".py"))               # Python only
         ds = ds.filter(lambda x: include(x["content"]))                   # DS imports only
@@ -29,7 +32,7 @@ class CleanDataset(IterableDataset):
         ds = ds.with_format("torch")
 
         self.ds = ds
-    
+
     def generate(self) -> Iterator[dict]:
         i = iter(self.ds)
         count = 0
@@ -51,8 +54,80 @@ class CleanDataset(IterableDataset):
                 continue
 
     def __iter__(self) -> Iterator[dict]:
-        return iter(self.generate())
-    
-    def get_dataloader(self, **kwargs) -> DataLoader:
-        return DataLoader(self, **kwargs)
+        return self.generate()
 
+
+class ChunkedDataset(CleanDataset):
+    def __init__(self, train_split: bool, max_size: int, tokenizer: AutoTokenizer,
+                 chunk_size: int = 256, chunk_overlap_len: int = 3, max_chunks: int = 128):
+
+        super().__init__(train_split, max_size)
+
+        self.tokenizer = tokenizer
+        self.chunk_size = chunk_size
+        self.overlapping_len = chunk_overlap_len
+        self.max_chunks = max_chunks
+
+    def generate(self) -> Iterator[dict]:
+        count = 0
+
+        for text in super().generate():
+            # Attempt to chunk each code sample
+            chunks = None
+            try:
+                chunks = chunk(inp=text["content"],
+                               tokenizer=self.tokenizer, 
+                               chunk_size=self.chunk_size,
+                               overlapping_len=self.overlapping_len,
+                               max_chunks=self.max_chunks)
+            except Exception as e:
+                print(f"[WARNING] Exception while chunking sample {count}/{self.max_size}: {e}. Skipped item")
+                continue
+
+            # Extract input ids and attention masks
+            ids, mask = chunks["input_ids"], chunks["attention_mask"]
+
+            # Yield each chunk, stopping if max_size is reached
+            for i in range(ids.size()[0]):
+                # Stop yielding if max_size is reached
+                if count >= self.max_size:
+                    break
+
+                # Yield
+                yield {
+                    "input_ids": ids[i],
+                    "attention_mask": mask[i]
+                }
+                count += 1
+
+            # Stop generating new chunks if max_size is reached
+            if count >= self.max_size:
+                break
+
+
+# SAMPLE USAGE
+if __name__ == "__main__":
+    tokenizer = AutoTokenizer.from_pretrained("./tokenizer_10M")
+
+    ds = ChunkedDataset(
+        train_split=True,      # Use training split
+        max_size=1_000_000,    # Provide up to 1 million samples (not files)
+        tokenizer=tokenizer,   # Set tokenizer
+        chunk_size=256,        # Max length of id/mask sequences is 256
+        chunk_overlap_len=3,   # Chunks share 3 ids with the previous chunk
+        max_chunks=128,        # Max chunks per file
+    )
+
+    # ChunkedDataset is iterable, so it can be directly passed to a DataLoader
+    from torch.utils.data import DataLoader
+
+    loader = DataLoader(
+        dataset=ds,
+        batch_size=16,
+        # shuffle should NOT be set because the dataset has unknown length
+    )
+
+    # Inspect a single element of this batch
+    for batch in loader:
+        print(batch)
+        break
