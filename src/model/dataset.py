0ad12a15a70533015272430d6bb808614c740567
Jeffrey Lu <jeffrey.p.lu@gmail.com>
2024-11-05 09:45:37 -0500

    ChunkedDataset

--- src/model/dataset.py
+++ src/model/dataset.py
@@ -1,8 +1,11 @@
 from datasets import load_dataset
 from torch.utils.data import DataLoader, IterableDataset
+from transformers import AutoTokenizer
 from typing import Iterator
 
 from preprocess import clean_comments, include, keep_only_content
+
+from chunk import chunk
 
 class CleanDataset(IterableDataset):
     TRAIN_SPLIT_NAME = "codeparrot/codeparrot-clean-train"
@@ -52,7 +55,51 @@ class CleanDataset(IterableDataset):
 
     def __iter__(self) -> Iterator[dict]:
         return iter(self.generate())
-    
-    def get_dataloader(self, **kwargs) -> DataLoader:
-        return DataLoader(self, **kwargs)
 
+
+class ChunkedDataset(CleanDataset):
+    def __init__(self, train_split: bool, max_size: int, tokenizer: AutoTokenizer,
+                 chunk_size: int = 256, chunk_overlap_len: int = 3, max_chunks: int = 128):
+        
+        super().__init__(train_split, max_size)
+
+        self.tokenizer = tokenizer
+        self.chunk_size = chunk_size
+        self.overlapping_len = chunk_overlap_len
+        self.max_chunks = max_chunks
+
+    def generate(self) -> Iterator[dict]:
+        count = 0
+
+        for text in super().generate():
+            # Attempt to chunk each code sample
+            chunks = None
+            try:
+                chunks = chunk(inp=text["content"],
+                               tokenizer=self.tokenizer, 
+                               chunk_size=self.chunk_size,
+                               overlapping_len=self.overlapping_len,
+                               max_chunks=self.max_chunks)
+            except Exception as e:
+                print(f"[WARNING] Exception while chunking sample {count}/{self.max_size}: {e}. Skipped item")
+                continue
+
+            # Extract input ids and attention masks
+            ids, mask = chunks["input_ids"], chunks["attention_mask"]
+
+            # Yield each chunk, stopping if max_size is reached
+            for i in range(ids.size()[0]):
+                # Stop yielding if max_size is reached
+                if count >= self.max_size:
+                    break
+
+                # Yield
+                yield {
+                    "input_ids": ids[i],
+                    "attention_mask": mask[i]
+                }
+                count += 1
+            
+            # Stop generating new chunks if max_size is reached
+            if count >= self.max_size:
+                break
